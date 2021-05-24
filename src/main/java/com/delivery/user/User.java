6756a878cea2301d9ad1d6e4adaab83cd6a5f6a8
soongjamm <kimstz0303@gmail.com>
2021-05-24 22:19:16 +0900

    style: 안쓰는 코드 정리

--- src/main/java/com/delivery/user/User.java
+++ src/main/java/com/delivery/user/User.java
@@ -22,31 +22,12 @@ public class User {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getNickname() {
         return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
