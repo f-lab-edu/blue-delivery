c7e17676c692709bd43ebcaae32325186fa1f87c
lyh7712 <81278286+lyh7712@users.noreply.github.com>
2021-06-17 16:28:20 +0900

    #39 메뉴 그룹 삭제 기능 구현 (#61)
    * feat: 메뉴 그룹 삭제 기능 구현
    
    * fix: 메뉴 그룹 삭제(Dto -> Id로 변경)
    
    * fix: 메뉴 그룹 삭제(null 체크 제거)

--- src/main/java/com/delivery/shop/menu/MenuGroupController.java
+++ src/main/java/com/delivery/shop/menu/MenuGroupController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,14 @@ public class MenuGroupController {
         this.service = service;
     }
 
+    /**
+     * 메뉴 그룹을 추가
+     *
+     *
+     * @param dto 가입할 메뉴 그룹 정보
+     * @return
+     */
+
     @PostMapping("/{shopId}/menu-groups")
     public ResponseEntity<MenuGroup> registerMenuGroup(@Validated @RequestBody MenuGroupDto dto,
                                                        @PathVariable Long shopId) {
@@ -34,6 +43,13 @@ public class MenuGroupController {
         service.registerMenuGroup(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    /**
+     * 메뉴 그룹 조회.
+     *
+     * @param shopId 매장 ID
+     * @return
+     */
 
     @GetMapping("/{shopId}/menu-groups")
     public ResponseEntity<List<MenuGroupDto>> getMenuGroups(@PathVariable Long shopId) {
@@ -46,6 +62,14 @@ public class MenuGroupController {
         return new ResponseEntity<List<MenuGroupDto>>(menuGroups, HttpStatus.OK);
     }
 
+    /**
+     * 메뉴 그룹 수정.
+     *
+     *
+     * @param dto 수정할 메뉴 그룹
+     * @return
+     */
+
     @PutMapping("/{shopId}/menu-groups")
     public ResponseEntity<MenuGroupDto> updateGroups(@PathVariable Long shopId,
                                                      @RequestBody MenuGroupDto dto) {
@@ -53,6 +77,21 @@ public class MenuGroupController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         service.updateMenuGroup(dto);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    /**
+     * 메뉴 그룹 삭제
+     *
+     *
+     * @param menuGroupId 삭제할 메뉴 그룹
+     * @return
+     */
+
+    @DeleteMapping("/{shopId}/menu-groups/{menuGroupId}")
+    public ResponseEntity<MenuGroupDto> deleteGroups(@PathVariable Long shopId,
+                                                     @PathVariable Long menuGroupId) {
+        service.deleteMenuGroup(menuGroupId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
