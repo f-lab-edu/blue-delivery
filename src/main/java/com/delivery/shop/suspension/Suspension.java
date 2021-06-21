127cbafae606bf5d6cc048c99f9ba1df46189b73
soongjamm <kimstz0303@gmail.com>
2021-06-21 15:11:47 +0900

    feature: 카테고리로 가게 조회시 영업일시중지 여부도 확인

--- src/main/java/com/delivery/shop/suspension/Suspension.java
+++ src/main/java/com/delivery/shop/suspension/Suspension.java
@@ -6,8 +6,33 @@ public class Suspension {
     private LocalDateTime from;
     private LocalDateTime to;
     
+    public Suspension() {
+        this.from = LocalDateTime.now();
+        this.to = from.plusNanos(0);
+    }
+    
     public Suspension(LocalDateTime from, LocalDateTime to) {
         this.from = from;
+        this.to = to;
+    }
+    
+    public boolean isSuspended(LocalDateTime now) {
+        return now.isAfter(from) && now.isBefore(to);
+    }
+    
+    public LocalDateTime getFrom() {
+        return from;
+    }
+    
+    public void setFrom(LocalDateTime from) {
+        this.from = from;
+    }
+    
+    public LocalDateTime getTo() {
+        return to;
+    }
+    
+    public void setTo(LocalDateTime to) {
         this.to = to;
     }
 }
