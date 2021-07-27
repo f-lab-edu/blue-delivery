dc510545bf5dc56e16a07f35ab54f0826095f1ae
soongjamm <kimstz0303@gmail.com>
2021-07-27 21:42:26 +0900

    fix: 한 요청내에서 @AuthenticationRequired로 인증객체를 한번 조회하면 ThreadLocal에 저장하여 재사용

---
+++ src/main/java/com/bluedelivery/api/authentication/AuthenticationHolder.java
@@ -0,0 +1,15 @@
+package com.bluedelivery.api.authentication;
+
+import com.bluedelivery.domain.authentication.Authentication;
+
+public class AuthenticationHolder {
+    private static ThreadLocal<Authentication> authentication = new ThreadLocal<>();
+    
+    public static Authentication getAuthentication() {
+        return authentication.get();
+    }
+    
+    public static void setAuthentication(Authentication auth) {
+        authentication.set(auth);
+    }
+}
