61667edad27dfadc57117d572f3f6973f5a63d79
soongjamm <kimstz0303@gmail.com>
2021-07-31 20:50:29 +0900

    #104 swagger 추가 (#105)
    * root package bluedelivery로 변경
    
    * common.authentication 패키지 정리
    
    * user package
    
    * api-application-domain-infra
    
    * config -> common.config
    
    * PasswordValidator를 controller와 같은 패키지로 이동
    
    * test package
    
    * (AddressService) application -> domain 으로 이동
    
    * category package
    
    * shop package
    
    * businesshour package
    
    * closingday package
    
    * menu 패키지 정리
    
    * api(menu) 패키지 분할
    
    * category adapter package
    
    * #101 @AuthenticationRequired + ArgumentResolver 이면 redis를 2번 조회하는 문제 (#102)
    
    * fix: 한 요청내에서 @AuthenticationRequired로 인증객체를 한번 조회하면 ThreadLocal에 저장하여 재사용
    
    * ThreadLocal을 검사하도록 수정
    
    * swagger 의존성, 기본 설정 추가
    
    * Swagger - Profile이 'prod'가 아닌 경우에만 동작하도록 설정
    
    Co-authored-by: lyh7712 <mmj2rv9@kakao.com>

---
+++ src/main/java/com/bluedelivery/common/config/Swagger2Config.java
@@ -0,0 +1,45 @@
+package com.bluedelivery.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.google.common.net.HttpHeaders;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+@Profile("!prod")
+public class Swagger2Config {
+    private static final String API_NAME = "Blue-Delivery API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "API 명세서";
+    
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+    
+}
