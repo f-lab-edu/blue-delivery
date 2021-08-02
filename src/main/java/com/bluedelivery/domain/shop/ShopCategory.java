94b593b7b7c1b6257a6d496fe73313e24378edbc
soongjamm <kimstz0303@gmail.com>
2021-08-02 18:26:58 +0900

    util: jib 설정 추가, redis 설정 수정, docker-compose 수정 등

--- src/main/java/com/bluedelivery/domain/shop/ShopCategory.java
+++ src/main/java/com/bluedelivery/domain/shop/ShopCategory.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.bluedelivery.domain.category.Category;
@@ -17,6 +18,7 @@ public class ShopCategory {
     private Long id;
     
     @ManyToOne
+    @JoinColumn(referencedColumnName = "SHOP_ID")
     private Shop shop;
     
     @ManyToOne
