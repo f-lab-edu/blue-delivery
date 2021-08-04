14bef45a73febb9a4928ccb7f848b6e0c279f27c
soongjamm <kimstz0303@gmail.com>
2021-08-05 03:14:11 +0900

    import 한줄 제거

--- src/main/java/com/bluedelivery/domain/shop/Shop.java
+++ src/main/java/com/bluedelivery/domain/shop/Shop.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.LongStream;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
