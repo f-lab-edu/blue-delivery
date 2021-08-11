0c7bdf47152847eff5bdd2414f0c6e78994cce0d
lyh7712 <mmj2rv9@kakao.com>
2021-08-11 19:35:55 +0900

    compose: Elastic APM 구성

--- src/main/java/com/bluedelivery/DeliveryApplication.java
+++ src/main/java/com/bluedelivery/DeliveryApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
+
 @SpringBootApplication
 @EnableCaching
 public class DeliveryApplication {
     public static void main(String[] args) {
+        ElasticApmAttacher.attach();
         SpringApplication.run(DeliveryApplication.class, args);
     }
 }
