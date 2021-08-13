41e17f4dcd19403c8c3dc2ffe0d89115ecfee8e6
lyh7712 <mmj2rv9@kakao.com>
2021-08-13 10:47:07 +0900

    리뷰 반영

--- src/main/java/com/bluedelivery/DeliveryApplication.java
+++ src/main/java/com/bluedelivery/DeliveryApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import co.elastic.apm.attach.ElasticApmAttacher;
 
 @SpringBootApplication
 @EnableCaching
 public class DeliveryApplication {
     public static void main(String[] args) {
-        ElasticApmAttacher.attach();
         SpringApplication.run(DeliveryApplication.class, args);
     }
 }
